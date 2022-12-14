3e73c04e0bbd3bb8d02f5019ff4011193d071e1a
dayi <yumibiao10@gmail.com>
2022-12-14 18:37:47 +0800

    test0.1

--- data/get_data.py
+++ data/get_data.py
@@ -6,6 +6,7 @@ import datetime
 from fake_useragent import UserAgent  #pip install fake-useragent
 
 import json
+import os
 
 
 d1 = datetime.timedelta(days=1) #天数变化值
@@ -16,30 +17,35 @@ for i in range(999999):
   url_str='https://2019ncov.chinacdc.cn/JKZX/yq_{}.json'.format(str_day)#生成url
   ua = UserAgent()
   headers={'User-Agent':ua.random}
-  
-  res = requests.get(url_str,headers=headers)
-  if res.status_code==200:
-    f=open("./data/"+str_day+".json","w")
-    j = json.loads(res.content) #读取json
-    f.write(json.dumps(j)) #写入json格式
-    f.close()
-  else:
-    attempt=0
-    while attempt<=3 and res.status_code!=200:
-      try:
-        res = requests.get(url_str,headers=headers)
-        if(res.status_code!=200):
-          print([401,"[dayi-error]"+str_day+" error:{}".format(attempt)])
-        attempt+=1
-      except:
-        print([501,"[dayi-error]"+str_day+" error:{}".format(attempt)])
-        attempt+=1
-        pass
-  
+  attempt=0
+  file_path = "./data/"+str_day+".json"
   date+=d1#天数加一天
   print(url_str)
   if date>=datetime.datetime.today():
     break
+  if os.path.exists(file_path):
+    if os.path.getsize(file_path)>=300*1024:
+      print([202,"[dayi-info]Found download file,skip it"])
+      continue
+  
+  
+  while attempt<=4:
+    try:
+      res = requests.get(url_str,headers=headers)
+      if res.status_code!=200:
+        print([401,"[dayi-error]"+str_day+" error:{}".format(attempt)])#未找到资源
+      if res.status_code==200:
+        f=open(file_path,"w")
+        j = json.loads(res.content) #读取json
+        f.write(json.dumps(j)) #写入json格式
+        f.close()
+        break
+    except :
+      print([501,"[dayi-error]"+str_day+" error:{}".format(attempt)])#程序内部错误，各种原因都可能
+      pass
+    attempt+=1
+  
+  
 
 
 # for i in range()
